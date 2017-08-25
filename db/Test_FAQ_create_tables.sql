510ccb17cf07ecd551e5a2931d5c4fb502a402f7
ukimiawz <deathmoon123>
2017-08-25 17:42:56 +0200

    Generated microservice version 1.0

---
+++ db/Test_FAQ_create_tables.sql
@@ -0,0 +1,17 @@
+--
+-- Database Schema:  faqSchema
+-- Automatically generated sql script for the service Test FAQ, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table faqTable.
+--
+CREATE TABLE faqSchema.faqTable (
+  question VARCHAR(255) ,
+  id INT NOT NULL AUTO_INCREMENT ,
+CONSTRAINT id_PK PRIMARY KEY (id)
+ 
+);
+
+
+
